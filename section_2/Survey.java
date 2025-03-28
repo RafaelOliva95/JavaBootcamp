3e300fb8ddff5389bb36932b2fa3d8b3bd2afc3d
Rafael Oliva <rafa.oliva950713@hotmail.com>
2025-03-28 13:29:41 -0600

    Adding last exercises for section 2 Variables

---
+++ section_2/Survey.java
@@ -0,0 +1,44 @@
+/* 03/27/2025
+ * author: Rafael Oliva
+ * Create an Instance of Scanner to read data from user
+ * Example using daily expenses data
+ */
+
+import java.util.Scanner;
+
+public class Survey {
+    public static void main(String[] args) {
+
+        //*********PART A: PICKING UP THE USER'S ANSWERS*********
+        System.out.println("Welcome. Thank you for taking the survey");
+        Scanner scanner = new Scanner(System.in); //Initialize Scanner interface
+        int counter = 0;
+        
+        System.out.println("\nWhat is your name?");
+        String name = scanner.nextLine();
+        counter++;
+
+        System.out.println("\nHow much money do you spend on coffee?");
+        double coffeePrice = scanner.nextDouble();
+        counter++;
+
+        System.out.println("\nHow much money do you spend on fast food?");
+        double foodPrice =  scanner.nextDouble();
+        counter++;
+
+        System.out.println("\nHow many times a week do you buy coffee?");
+        int coffeeAmount = scanner.nextInt();
+        counter++;
+
+        System.out.println("\nHow many times a week do you buy fast food?");
+        int foodAmount = scanner.nextInt();
+        counter++;
+
+        scanner.close(); // !Always close the scanner if not you will get a resource leak
+        //*********PART B: RESPONDING TO THE USER**********
+        System.out.println("\n\nThank you " + name + " for answering all " + counter + " questions");
+        System.out.println("Weekly, you spend $" + (coffeeAmount * coffeePrice)+ " on coffee");
+        System.out.println("Weekly, you spend $" + (foodAmount * foodPrice) + " on food");
+
+    }
+}
