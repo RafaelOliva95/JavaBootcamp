1181fb55704414a96212978b7432152fc469124b
Rafael Oliva <rafa.oliva950713@hotmail.com>
2025-04-12 23:42:30 -0600

    Add new excercise of Booleans and Conditionals

---
+++ section_3/JavaMart.java
@@ -0,0 +1,37 @@
+public class JavaMart {
+    public static void main(String[] args) {
+        double wallet = 101.98;
+
+        //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
+
+ 
+        double toyCar = 5.99;
+        System.out.println("Can I get this car?");
+       
+        //if you have enough money
+            //       print: Sure!
+            //       pay for the toy car 
+
+        //else:  Sorry, I only have <wallet> left.
+        if(toyCar <= wallet){
+            System.out.println("Sure!");
+            wallet -= toyCar;
+        }else{
+            System.out.println("Sorry, I only have " + wallet + " left.");
+        }
+        
+        double nike = 95.99;
+        System.out.println("Can I get these nike shoes?");
+        //if you have enough money
+            //       print: Sure!
+            //       pay for the nike shoes 
+
+        //else: Sorry, I only have <wallet> left.
+        if(nike <= wallet){
+            System.out.println("Sure!");
+            wallet -= nike;
+        }else{
+            System.out.println("Sorry, I only have " + wallet + " left.");
+        }
+    }
+}
